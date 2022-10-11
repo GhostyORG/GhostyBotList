e6790f00385285b5ec0b24b05f36e3410bfa16f7
Ryder <78247679+killerclaws12@users.noreply.github.com>
2022-10-10 21:32:54 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,58 @@
+module.exports = {
+    maintenance: false,
+    mongodb: "",
+    bot: {
+        id: "",
+        token: "",
+        channels: {
+            login: "",
+            vote: "",
+            error: "",
+            info: "",
+            new: "",
+            admin: "",
+            welcome: ""
+        },
+        bsl: {
+            id: "",
+            token: "",
+            channels: {
+                vote: "",
+                error: "",
+            },
+            invite: "https://discord.com/api/oauth2/authorize?client_id=1006333261192106144&permissions=314433&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth%2Fcallback&response_type=code&scope=applications.commands%20bot"
+        }
+    },
+    discord: {
+        id: "1029143113366765608",
+        invite: "https://discord.gg/AZe2sExMrE",
+        roles: {
+            admin: "",
+            mod: "",
+            user: "",
+            bot: ""
+        }
+    },
+    server: {
+        port: 3000,
+        host: "http://localhost:3000",
+        tags: [
+            "development",
+            "fun",
+            "media",
+            "company",
+            "game",
+            "emoji",
+            "turkish",
+            "support",
+            "sound",
+            "chatting",
+            "nsfw",
+            "challange"
+        ]
+    },
+    users: {
+        owner: ["719660045817872394"],
+        verificator: ["614841986289106957", "774553653394538506", "386421496463097857", "996544959127752855"],
+    }
+}
